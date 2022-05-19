58568211d86e981e63c85b16f4824b4b4e15398e
shafoo98 <67111399+shafoo98@users.noreply.github.com>
2022-05-19 20:00:28 +0600

    Deployment

--- frontend/src/screens/AboutUsScreen.js
+++ frontend/src/screens/AboutUsScreen.js
@@ -5,41 +5,7 @@ const AboutUsScreen = () => {
   return (
     <Container>
       <h1 style={{ height: '7.5vh' }}>About Us: </h1>
-      <h5 style={{ fontSize: '1.25rem' }}>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque unde
-        fugiat ea in impedit eius. Expedita recusandae laudantium autem
-        reiciendis beatae repellat harum rerum dicta placeat, quisquam unde
-        possimus impedit. Adipisci veritatis quos tenetur unde amet fuga
-        repellat? Eos fugit quibusdam obcaecati iusto libero, quam molestias in
-        maiores officiis ipsum deleniti rerum minus quas quidem debitis, tempora
-        magni? Consequatur, accusantium? Pariatur voluptas doloribus, totam
-        voluptates explicabo deleniti et maiores? Laborum deleniti fugiat sit
-        consectetur dolor totam id tempora est ducimus optio ipsa facilis neque
-        nostrum pariatur nobis, quia mollitia quo? Numquam, cupiditate
-        voluptatum. Eaque vero saepe autem iure sed, quis iste cupiditate nulla
-        magnam totam deserunt voluptatibus. Fuga ducimus quisquam aspernatur
-        delectus itaque error qui animi repudiandae modi officia! Omnis. Sit
-        eius rerum ex blanditiis laborum consequuntur totam. Perferendis
-        aspernatur voluptate, illo dolor maxime earum. Officiis dolores, beatae,
-        est soluta maiores illo aut rem nemo vitae doloribus, eum velit magni?
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate in
-        architecto ipsa est officiis quas. Quis ipsam id repudiandae ipsa quos
-        ducimus, voluptas officia vel veritatis, repellendus harum, cumque
-        consectetur. Tenetur cumque corporis magnam temporibus cum, dolore
-        dolorum officia earum libero. Eius commodi velit eum dolorum, alias
-        perferendis magnam debitis blanditiis, provident tenetur laborum, nemo
-        consequuntur quos cumque consequatur quia? Veniam nostrum ea velit
-        repellat quae placeat deserunt perferendis repellendus! Qui optio est
-        perferendis nisi quibusdam temporibus. Quam, dolore facere sunt
-        incidunt, explicabo laudantium minus nihil voluptates eius facilis
-        quidem! Soluta harum magnam quidem temporibus ipsum nostrum ducimus
-        excepturi corporis dolor autem! Aspernatur error atque officiis sed
-        aliquam dolore eius architecto voluptatum. Accusamus, ex fugit. At eaque
-        tenetur doloribus perferendis! Dicta vitae vel dolor animi voluptates
-        enim odit veniam? Ipsum eaque modi nemo veniam dolorum, debitis eum
-        saepe ratione aspernatur corrupti quae itaque adipisci necessitatibus
-        consectetur. Iste cum aut asperiores?
-      </h5>
+      <h5 style={{ fontSize: '1.25rem' }}></h5>
       <h1 style={{ height: '7.5vh' }}>Contact Us: </h1>
       <p>
         Contact us at our <a href='www.facebook.com'>page</a> for any of your
