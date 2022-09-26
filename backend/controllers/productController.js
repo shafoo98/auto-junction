9971738b9da2401ab95a0b7492bb473797302bd5
shafoo98 <67111399+shafoo98@users.noreply.github.com>
2022-09-27 01:36:55 +0600

    ""

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -7,7 +7,7 @@ import Product from "../models/productModel.js";
 // @route GET /api/products
 // @access Public
 const getProducts = asyncHandler(async (req, res) => {
-  const pageSize = 6;
+  const pageSize = 100;
   const page = Number(req.query.pageNumber) || 1;
 
   const keyword = req.query.keyword
